e474e3260611c3ec24ebff4675d8320fa9491ab2
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-04-04 21:54:12 -0300

    feat: database interligation uncomplete

---
+++ backend/1649113663780-CreateAppointments.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export default class CreateAppoinments1648769785348 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: "appointments",
+          columns: [
+            {
+              name: "id",
+              type: "varchar",
+              isPrimary: true,
+              generationStrategy: "uuid",
+            },
+            {
+              name: "provider",
+              type: "varchar",
+              isNullable: false,
+            },
+            {
+              name: "date",
+              type: "typestamp with time zone", //funciona só no postgres
+              //Caso fosse fazer em qualquer outro banco de dados
+              //Utilizar only typestamp
+              isNullable: false,
+            }
+          ]
+        })
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("appointments")
+    }
+
+}
