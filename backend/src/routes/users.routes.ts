7527c39da7a72b5401693ae140f0b110d22fbc23
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-04-26 20:53:46 -0300

    feat: static files and update avatar functions

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer'
 import uploadConfig from "../config/upload";
 
 import CreateUserService from "../services/CreateUsersServices";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
 
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
@@ -32,9 +33,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async(request, response) => {
-  console.log(request.file)
+  try{
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-  return response.json({ok:true})
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    })
+
+    delete user.password;
+
+    return response.json(user)
+  }catch (error ){
+    return response.status(400).json({error: error.message})
+  }
 })
 
 export default usersRouter;
