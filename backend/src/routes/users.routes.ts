cb2eb765aa0877f33ceead5e2119b47df6429cad
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-04-25 21:06:04 -0300

    feat: files's upload system

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from 'multer'
+import uploadConfig from "../config/upload";
 
 import CreateUserService from "../services/CreateUsersServices";
 
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -20,6 +25,16 @@ usersRouter.post("/", async (request, response) => {
   } catch (error) {
     return response.status(400).json({error: error.message})
   }
+})
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async(request, response) => {
+  console.log(request.file)
+
+  return response.json({ok:true})
 })
 
 export default usersRouter;
