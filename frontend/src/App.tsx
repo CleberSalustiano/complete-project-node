f2d72fb945a39775b1e87d8a77688feb4896190e
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-05-18 20:48:38 -0300

    feat: SignUp created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 // App.tsx
 import React from 'react';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 import GlobalStyled from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyled />
   </>
 );
